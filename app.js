24f219191611425be57aebf3c5786b777ffc6839
Ajay NS <ajay.ns@outlook.com>
2017-02-02 16:44:27 +0530

    Add readme

---
+++ app.js
@@ -0,0 +1,35 @@
+var app = angular.module('movBro', []);
+
+app.controller('MainCtrl', function($scope, $http) {
+
+  $scope.$watch('searchTerm', function(){
+    fetch();
+  });
+
+  $scope.searchTerm = "The Dark Knight";
+
+  function fetch(){
+    $http.get("https://www.omdbapi.com/?t=" + $scope.searchTerm + "&tomatoes=true&plot=full")
+      .then(function(response){ $scope.details = response.data });
+
+    $http.get("https://www.omdbapi.com/?s=" + $scope.searchTerm)
+      .then(function(response){ $scope.related = response.data });
+  }
+
+  $scope.select = function() {
+    this.setSelectionRange(0, this.value.length);
+  };
+
+  $scope.update = function(movie) {
+    $scope.searchTerm = movie.Title;
+  };
+});
+
+app.directive('ngBg', function() {
+  return function(scope, element, attrs) {
+    var url = attrs.ngBg;
+    element.css({
+      'background-image': 'url(' + url + ')'
+    });
+  };
+});
---
+++ readme.md
@@ -0,0 +1,9 @@
+# OMDb Movie Browser
+
+Imdb redesigned with AngularJS + Semantic UI. 
+It basically illustrates the use of OMDb API along with Angular JS to make a webapp. 
+
+
+### Installation
+Just clone and run from your browser
+
